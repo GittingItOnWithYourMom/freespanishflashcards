bef0d1c3cadcfcc49e0a8cc394f82cfe77fb9825
GittingItOnWithYourMom <146044625+GittingItOnWithYourMom@users.noreply.github.com>
2024-10-07 21:53:06 -0700

    physics class

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ const theme = document.getElementById("theme")
 const body = document.body
 const darktext = document.querySelectorAll(".darkmode")
 const backdoor = document.getElementById('backdoor')
-const ball = document.getElementById('ball')
+const ballimage = document.getElementById('ball')
+const boximage = document.getElementById('box')
 
 function start(game){
     removeIframe()
@@ -91,53 +92,90 @@ function hideball(){
 }
 
 //physics
-var framerate = 10
-var bottom = 845
-var wall = 1802
-var physics_objects = [ball]
-var deltaTX = 0
-var deltaTY = 0
-var posY = 0
-var posX = 0
-var initialY = 0
-var initialX = 0
+
+class physicsobject{
+    constructor(image, initialX, initialY, posX, posY, velX, velY, accX, accY, deltaTX, deltaTY){
+        this.image = image
+        this.initialX = initialX
+        this.initialY = initialY
+        this.posX = posX
+        this.posY = posY
+        this.velX = velX
+        this.velY = velY
+        this.accX = accX
+        this.accY = accY
+        this.deltaTX = deltaTX
+        this.deltaTY = deltaTY
+    }
+    moveImage(){
+        var amount = "translate(" +this.posX+ "px, " +this.posY+ "px)"
+        this.image.style.transform = amount
+    }
+}
+
+var ball = new physicsobject(ballimage, 100, 100, 100, 100, 100, 0, 0, 0, 0, 0)
+var box = new physicsobject(boximage, 900, 0, 900, 0, 0, 0, 0, 0, 0, 0)
+
+var framerate = 1
+var physics_objects = [ball, box]
+var prevScroll
+var newScroll
+var prevWindow
+var newWindow
+
 var friction = 0
 setInterval(() => {
+    prevScroll = newScroll
+    newScroll = scrollY
+    var accBorder = scrollY - prevScroll
+    prevWindow = newWindow
+    newWindow = innerHeight
+    var accWindow = innerHeight - prevWindow
+    console.log(accWindow);
+
     physics_objects.forEach((object) => {
-        //Y
-        if (posY < bottom + scrollY){
-            friction = 1
-            var accY = 4.8
-            var velY = 0
-            posY = initialY + velY*deltaTY + accY*deltaTY*deltaTY * 0.01
-            
-            var amount = "translate(" +0+ "px, " +posY+ "px)"
-            object.style.transform = amount
-            deltaTY += 1
+        var bottom = window.innerHeight - 105
+        var wall = window.innerWidth - 120
+        var offset = 0
+        const limiter = 0.00001
+
+        object.accY = 9.8
+
+        object.initialY = object.posY
+        object.initialX = object.posX
+        if (object.posY > bottom + scrollY){
+            object.accY -= object.accY
+            object.velY -= object.velY
+            object.deltaTY = 0
         }
-        else{
-            friction = 0.001
-            deltaTY = 0
-            initialY = posY
-            posY = bottom + scrollY
-            var amount = "translate(" +0+ "px, " +posY+ "px)"
-            object.style.transform = amount
+        if (object.posX > wall){
+            object.accX -= object.accX
+            object.velX -= object.velX
+            object.deltaTX = 0
         }
-        //X
-        if (posX < wall){
-            var accX = 0
-            var velX = 4
-            posX = initialX + velX*deltaTX + accX*deltaTX*deltaTX
-            var amount = "translate(" +posX+ "px, " +posY+ "px)"
-            object.style.transform = amount
-            deltaTX += 1
+        if (accBorder < 0){
+            offset += accBorder
         }
-        else{
-            deltaTX = 0
-            initialX = posX
-            posX = wall
-            var amount = "translate(" +posX+ "px, " +posY+ "px)"
-            object.style.transform = amount
+        if (accWindow < 0){
+            offset += accWindow
         }
+        object.posY = object.initialY + object.velY*object.deltaTY*limiter + object.accY*object.deltaTY*object.deltaTY*limiter + offset
+        object.posX = object.initialX + object.velX*object.deltaTX*limiter + object.accX*object.deltaTX*object.deltaTX*limiter
+        object.deltaTY += 1
+        object.deltaTX += 1
+        object.moveImage()
     })
 }, framerate)
+
+function moveobject(object){
+    setTimeout(() => {
+        console.log(`Mouse X: ${mousex}, Mouse Y: ${mousey}`)
+    },10)
+}
+
+var mousex
+var mousey
+document.addEventListener('mousemove', (event) => {
+    mousex = event.clientX;
+    mousey = event.clientY;
+})
